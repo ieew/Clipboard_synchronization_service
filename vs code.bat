4b00e2ed94ece8228724e1ce30e65eeb722d7d10
ieew <44091335+ieew@users.noreply.github.com>
2020-12-14 23:33:35 +0800

    Delete vs code.bat

--- vs code.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-color b
-mode con cols=45 lines=10
-set "PYTHON_PATH=Q:\Python38"
-
-set "PATH=C:\bash\bin;C:\bash\lib;C:\bash\share;%PYTHON_PATH%;%PYTHON_PATH%\Scripts;%PYTHON_PATH%\DLLs;%PATH%"
-start "" "D:\data\VSCode\Code.exe" "%~dp0"
